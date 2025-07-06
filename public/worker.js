696136faf07227c89450f56b4df5c24e48d68834
Adrià <adriablancafort@gmail.com>
2025-07-06 14:43:48 +0200

    redirect to install page

--- public/worker.js
+++ public/worker.js
@@ -1,5 +1,8 @@
-// Redirect to feedback page on uninstall
-chrome.runtime.onInstalled.addListener(() => {
+chrome.runtime.onInstalled.addListener((details) => {
+  if (details.reason === 'install') {
+    chrome.tabs.create({ url: 'https://pricecomparison.fyi/extension-install' });
+  }
+
   chrome.runtime.setUninstallURL('https://pricecomparison.fyi/extension-uninstall');
 });
 

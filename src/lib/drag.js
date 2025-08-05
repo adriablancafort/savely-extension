db1e6441681da34aec605d929d96c4639d6677b8
Adrià <adriablancafort@gmail.com>
2025-08-05 22:06:56 +0200

    correct storage imports

--- src/lib/drag.js
+++ src/lib/drag.js
@@ -1,5 +1,5 @@
+import { getValue, setValue } from '$lib/storage.js';
 import { draggable } from '@neodrag/svelte';
-import { getValue, setValue } from './storage.js';
 
 export async function drag(node, { key, ...options } = {}) {
   const position = await getValue(key) || { x: 0, y: 0 };
